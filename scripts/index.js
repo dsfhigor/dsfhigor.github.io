4375474ea37a3b80b993d572fd2a6be680a22ba1
Hígor da Silva Flores <dsfhigor@gmail.com>
2023-10-02 22:50:00 -0300

    att-button
    Atualização de codigo do botao de home page: estilização e funcionamento.

--- scripts/index.js
+++ scripts/index.js
@@ -4,18 +4,18 @@ let mybutton = document.getElementById("btnHome");
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
         mybutton.style.display = "block";
     } else {
         mybutton.style.display = "none";
     }
 }
-
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
 const darkModeToggle = document.getElementById('darkModeToggle');
 const body = document.body;
 const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
